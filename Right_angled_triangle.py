fd109647535ae77f9b1495938db07ff994bc748f
ahronSteinbruch <139044285+ahronSteinbruch@users.noreply.github.com>
2025-06-25 09:25:24 +0300

    create Right-angled triangle ineraete from rectangle

---
+++ Right_angled_triangle.py
@@ -0,0 +1,13 @@
+from Rectangle import Rectangle
+class Right_angled_triangle(Rectangle):
+    def __init__(self, height, width):
+        super().__init__(height, width)
+
+    def get_area(self):
+        return super().get_area()/2
+
+    def get_perimeter(self):
+        return super().get_perimeter()/2
+
+    def __str__(self):
+        return f"Right_angled_triangle: height = {self.height}, width = {self.width}, area = {self.get_area()}, perimeter = {self.get_perimeter()}"
